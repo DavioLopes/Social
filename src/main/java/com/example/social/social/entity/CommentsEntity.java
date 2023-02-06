eb74ad6f001e1ed7fe0f6c67979abd707fba3461
Davio Lopes <davioplp@gmail.com>
2023-02-06 19:42:28 -0300

    feat: CommentsEntity

---
+++ src/main/java/com/example/social/social/entity/CommentsEntity.java
@@ -0,0 +1,35 @@
+package com.example.social.social.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "comments")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CommentsEntity implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+
+  @Column(name = "textComment")
+  private String textComment;
+
+  @CreatedDate
+  @Column(nullable = false, name = "createdAt")
+  private Instant createdDate;
+}
