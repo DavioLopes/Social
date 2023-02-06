679cbd7b672f57bbdfeb2ef0a343105df7151b48
Davio Lopes <davioplp@gmail.com>
2023-02-06 19:40:22 -0300

    feat: LikesEntity

---
+++ src/main/java/com/example/social/social/entity/LikesEntity.java
@@ -0,0 +1,27 @@
+package com.example.social.social.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serial;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "likes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LikesEntity implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+
+}
